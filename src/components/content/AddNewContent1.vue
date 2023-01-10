5cf83732949d364bf535a3f9269ef4f0890f896b
knhmb <khaled@marvelconnect.com>
2023-01-10 14:49:06 +0800

    Modificatons

--- src/components/content/AddNewContent1.vue
+++ src/components/content/AddNewContent1.vue
@@ -40,7 +40,49 @@
           </div>
         </el-col>
       </el-row>
-      <div class="content-card"></div>
+      <div class="content-card">
+        <el-row style="align-items: center">
+          <el-col :span="3"> </el-col>
+          <el-col :span="18">
+            <div class="content-box">
+              <div class="top">
+                <img src="../../assets/url-txt.png" alt="" />
+                <el-input v-model="input1"></el-input>
+              </div>
+              <div class="bottom">
+                <el-upload
+                  v-model:file-list="fileList"
+                  class="upload-demo"
+                  action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
+                  multiple
+                  :on-preview="handlePreview"
+                  :on-remove="handleRemove"
+                  :before-remove="beforeRemove"
+                  :limit="3"
+                  :on-exceed="handleExceed"
+                >
+                  <el-button type="primary">Click to upload</el-button>
+                  <!-- <template #tip>
+                    <div class="el-upload__tip">
+                      jpg/png files with a size less than 500KB.
+                    </div>
+                  </template> -->
+                </el-upload>
+              </div>
+            </div>
+          </el-col>
+          <el-col :span="3" style="text-align: end">
+            <img
+              @mouseover="changeIcon"
+              @mouseout="defaultIcon"
+              class="delete-icon"
+              @click="dialogDelete = true"
+              :src="deleteIcon"
+              alt=""
+            />
+          </el-col>
+        </el-row>
+      </div>
     </div>
   </div>
 </template>
@@ -65,6 +107,8 @@ export default {
       dialogVisible: false,
       dialogDelete: false,
       upload: "",
+      input1: "What travel is really like in 2020",
+      deleteIcon: require("../../assets/del@2x.png"),
     };
   },
   methods: {
@@ -76,6 +120,12 @@ export default {
       //   const file = files[0];
       //   file.uid = genFileId;
       //   this.upload.handleStart(file);
+    },
+    changeIcon() {
+      this.deleteIcon = require("../../assets/del.png");
+    },
+    defaultIcon() {
+      this.deleteIcon = require("../../assets/del@2x.png");
     },
   },
 };
@@ -147,5 +197,77 @@ export default {
   border-radius: 12px;
   padding: 1rem;
   margin-top: 1rem;
+}
+
+.add-new-content .content-card .content-box {
+  border: 1px solid #262626;
+  border-radius: 12px;
+}
+
+.add-new-content .content-card .content-box .top {
+  border-bottom: 1px solid #262626;
+  padding: 0.8rem 2.5rem;
+  position: relative;
+}
+
+.add-new-content
+  .content-card
+  .content-box
+  :deep(.el-input .el-input__wrapper) {
+  box-shadow: none;
+  background: transparent;
+}
+
+.add-new-content .content-card .content-box .top img {
+  width: 1.7rem;
+  position: absolute;
+  top: 47%;
+  left: 0.5rem;
+  transform: translateY(-50%);
+}
+
+.add-new-content .content-card .content-box .bottom :deep(.upload-demo) {
+  display: flex;
+}
+.add-new-content
+  .content-card
+  .content-box
+  .bottom
+  :deep(.el-upload-list.el-upload-list--text) {
+  width: 100%;
+}
+
+.add-new-content
+  .content-card
+  .content-box
+  .bottom
+  :deep(.el-button.el-button--primary) {
+  background: #d5d5d7;
+  border: 1px solid #262626;
+  border-radius: 20px;
+  font-family: "Poppins";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 20px;
+  color: #262626;
+}
+
+.add-new-content .content-card .content-box .bottom {
+  padding: 0.8rem 0.5rem;
+}
+
+.add-new-content .delete-icon {
+  width: 2rem;
+  cursor: pointer;
+  background: #e9e9eb;
+  border: 1px solid #262626;
+  border-radius: 20px;
+  padding: 0.1rem;
+}
+
+.add-new-content .delete-icon:hover {
+  background: #f03738;
+  border-color: #f03738;
 }
 </style>
